508f32ce95feb1b306dc2269fe965fa88fc408c2
Silind <silindsoftwaredk@gmail.com>
2019-11-11 21:02:30 +0100

    Dynamic OG

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -7,25 +7,26 @@ interface IProps {
 
 const AppHeader: FC<IProps> = (props) => {
   const title = props.title || 'Direflow';
+  const description = 'Create Web Components using React';
 
   return (
     <Helmet>
       <meta charSet='utf-8' />
 
       <meta property='og:type' content='”website”' />
-      <meta property='og:site_name' content='Direflow' />
+      <meta property='og:site_name' content={title} />
       <meta property='og:url' content='https://direflow.io/' />
       <meta
         property='og:image'
         content='https://silind-s3.s3.eu-west-2.amazonaws.com/direflow/og-image.png'
       />
-      <meta property='og:description' content='Direflow' />
+      <meta property='og:description' content={description} />
 
       <meta name='twitter:card' content='summary_large_image' />
       <meta name='twitter:site' content='@silindsoftware' />
       <meta name='twitter:creator' content='@silindsoftware' />
-      <meta name='twitter:title' content='Direflow' />
-      <meta name='twitter:description' content='Create Web Components using React' />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
       <meta
         name='twitter:image'
         content='https://silind-s3.s3.eu-west-2.amazonaws.com/direflow/og-image.png'
